c53c382b4b29bd2ec2f918a4b5eff6c1a25c7f33
GoldenDragonDesigner <SokaGameDesign@gmail.com>
2020-03-26 14:58:33 -0700

    Adding the saving and loading to the project

---
+++ SevenVibrations/Assets/Saving&Loading/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//for creating a SAVE & LOAD SYSTEM in unity by Brackeys
+[System.Serializable]
+public class PlayerData : MonoBehaviour
+{
+    public int savedNumOfObjects;
+    public float health;
+    public float[] position;
+
+
+    public PlayerData(Player player)
+    {
+        savedNumOfObjects = player.numberOfObjectsGlowing;
+        health = player.curHeatlh;
+
+        position = new float[3];
+        position[0] = GlobalVariables.PLAYER.transform.position.x;
+        position[1] = GlobalVariables.PLAYER.transform.position.y;
+        position[2] = GlobalVariables.PLAYER.transform.position.z;
+    }
+
+}
---
+++ SevenVibrations/Assets/Saving&Loading/ReadMe.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadMe : MonoBehaviour
+{
+        /*
+     * If you have any interest in where I got my information or are wondering how the layout
+     * of this gam is I will continiously update this document with information regarding how I 
+     * went about creating my first game solo.  
+     * 
+     * 2/26/2020 The very first thing I did was to test and see if the mechanic I am thinking about creating
+     * is feasible.  I first created the voice mechanic for this game.  
+     * 
+     * 3/7/2020 Now I am working on the Save feature for the game. I am thinking of just making it automatic.  
+     * 
+     * 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    */
+}
