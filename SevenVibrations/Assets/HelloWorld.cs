236bea0df8d6544f6cf18ea0589750cfad472458
GoldenDragonDesigner <SokaGameDesign@gmail.com>
2020-03-26 17:24:25 -0700

    adding the rest of the assets

--- SevenVibrations/Assets/HelloWorld.cs
+++ SevenVibrations/Assets/HelloWorld.cs
@@ -1,136 +1,136 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using Microsoft.CognitiveServices.Speech;
-#if PLATFORM_ANDROID
-using UnityEngine.Android;
-#endif
-#if PLATFORM_IOS
-using UnityEngine.iOS;
-using System.Collections;
-#endif
+﻿//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using Microsoft.CognitiveServices.Speech;
+//#if PLATFORM_ANDROID
+//using UnityEngine.Android;
+//#endif
+//#if PLATFORM_IOS
+//using UnityEngine.iOS;
+//using System.Collections;
+//#endif
 
 
-public class HelloWorld : MonoBehaviour
-{
-    public Text outputText;
-    public Button startRecoButton;
+//public class HelloWorld : MonoBehaviour
+//{
+//    public Text outputText;
+//    public Button startRecoButton;
 
-    private object threadLocker = new object();
-    private bool waitingForReco;
-    private string message;
+//    private object threadLocker = new object();
+//    private bool waitingForReco;
+//    private string message;
 
-    private bool micPermissionGranted = false;
+//    private bool micPermissionGranted = false;
 
-#if PLATFORM_ANDROID || PLATFORM_IOS
-    private Microphone mic;
-#endif
+//#if PLATFORM_ANDROID || PLATFORM_IOS
+//    private Microphone mic;
+//#endif
 
-    public async void ButtonClick()
-    {
-        var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "westus");
+//    public async void ButtonClick()
+//    {
+//        var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "westus");
 
-        using (var recognizer = new SpeechRecognizer(config))
-        {
-            lock (threadLocker)
-            {
-                waitingForReco = true;
-            }
+//        using (var recognizer = new SpeechRecognizer(config))
+//        {
+//            lock (threadLocker)
+//            {
+//                waitingForReco = true;
+//            }
 
-            // Starts speech recognition, and returns after a single utterance is recognized. The end of a
-            // single utterance is determined by listening for silence at the end or until a maximum of 15
-            // seconds of audio is processed.  The task returns the recognition text as result.
-            // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
-            // shot recognition like command or query.
-            // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
-            var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
+//            // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+//            // single utterance is determined by listening for silence at the end or until a maximum of 15
+//            // seconds of audio is processed.  The task returns the recognition text as result.
+//            // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+//            // shot recognition like command or query.
+//            // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+//            var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
 
-            // Checks result.
-            string newMessage = string.Empty;
-            if (result.Reason == ResultReason.RecognizedSpeech)
-            {
-                newMessage = result.Text;
-            }
-            else if (result.Reason == ResultReason.NoMatch)
-            {
-                newMessage = "NOMATCH: Speech could not be recognized.";
-            }
-            else if (result.Reason == ResultReason.Canceled)
-            {
-                var cancellation = CancellationDetails.FromResult(result);
-                newMessage = $"CANCELED: Reason={cancellation.Reason} ErrorDetails={cancellation.ErrorDetails}";
-            }
+//            // Checks result.
+//            string newMessage = string.Empty;
+//            if (result.Reason == ResultReason.RecognizedSpeech)
+//            {
+//                newMessage = result.Text;
+//            }
+//            else if (result.Reason == ResultReason.NoMatch)
+//            {
+//                newMessage = "NOMATCH: Speech could not be recognized.";
+//            }
+//            else if (result.Reason == ResultReason.Canceled)
+//            {
+//                var cancellation = CancellationDetails.FromResult(result);
+//                newMessage = $"CANCELED: Reason={cancellation.Reason} ErrorDetails={cancellation.ErrorDetails}";
+//            }
 
-            lock (threadLocker)
-            {
-                message = newMessage;
-                waitingForReco = false;
-            }
-        }  
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        if(outputText == null)
-        {
-            UnityEngine.Debug.LogError("outputText property is null! Assign a UI Text element to it.");
-        }
-        else if(startRecoButton == null)
-        {
-            message = "startRecoButton property is null! Assign a UI Button to it.";
-            UnityEngine.Debug.LogError(message);
-        }
-        else
-        {
-            // Continue with normal initialization, Text and Button objects are present.
-#if PLATFORM_ANDROID
-            // Request to use the microphone, cf.
-            // https://docs.unity3d.com/Manual/android-RequestingPermissions.html
-            message = "Waiting for mic permission";
-            if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
-            {
-                Permission.RequestUserPermission(Permission.Microphone);
-            }
-#elif PLATFORM_IOS
-            if (!Application.HasUserAuthorization(UserAuthorization.Microphone))
-            {
-                Application.RequestUserAuthorization(UserAuthorization.Microphone);
-            }
-#else
-            micPermissionGranted = true;
-            message = "Click button to recognize speech";
-#endif
-            startRecoButton.onClick.AddListener(ButtonClick);
-        }
-    }
+//            lock (threadLocker)
+//            {
+//                message = newMessage;
+//                waitingForReco = false;
+//            }
+//        }  
+//    }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        if(outputText == null)
+//        {
+//            UnityEngine.Debug.LogError("outputText property is null! Assign a UI Text element to it.");
+//        }
+//        else if(startRecoButton == null)
+//        {
+//            message = "startRecoButton property is null! Assign a UI Button to it.";
+//            UnityEngine.Debug.LogError(message);
+//        }
+//        else
+//        {
+//            // Continue with normal initialization, Text and Button objects are present.
+//#if PLATFORM_ANDROID
+//            // Request to use the microphone, cf.
+//            // https://docs.unity3d.com/Manual/android-RequestingPermissions.html
+//            message = "Waiting for mic permission";
+//            if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+//            {
+//                Permission.RequestUserPermission(Permission.Microphone);
+//            }
+//#elif PLATFORM_IOS
+//            if (!Application.HasUserAuthorization(UserAuthorization.Microphone))
+//            {
+//                Application.RequestUserAuthorization(UserAuthorization.Microphone);
+//            }
+//#else
+//            micPermissionGranted = true;
+//            message = "Click button to recognize speech";
+//#endif
+//            startRecoButton.onClick.AddListener(ButtonClick);
+//        }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-#if PLATFORM_ANDROID
-        if (!micPermissionGranted && Permission.HasUserAuthorizedPermission(Permission.Microphone))
-        {
-            micPermissionGranted = true;
-            message = "Click button to recognize speech";
-        }
-#elif PLATFORM_IOS
-        if (!micPermissionGranted && Application.HasUserAuthorization(UserAuthorization.Microphone))
-        {
-            micPermissionGranted = true;
-            message = "Click button to recognize speech";
-        }
-#endif
+//    // Update is called once per frame
+//    void Update()
+//    {
+//#if PLATFORM_ANDROID
+//        if (!micPermissionGranted && Permission.HasUserAuthorizedPermission(Permission.Microphone))
+//        {
+//            micPermissionGranted = true;
+//            message = "Click button to recognize speech";
+//        }
+//#elif PLATFORM_IOS
+//        if (!micPermissionGranted && Application.HasUserAuthorization(UserAuthorization.Microphone))
+//        {
+//            micPermissionGranted = true;
+//            message = "Click button to recognize speech";
+//        }
+//#endif
 
-        lock (threadLocker)
-        {
-            if (startRecoButton != null)
-            {
-                startRecoButton.interactable = !waitingForReco && micPermissionGranted;
-            }
-            if (outputText != null)
-            {
-                outputText.text = message;
-            }
-        }
-    }
-}
+//        lock (threadLocker)
+//        {
+//            if (startRecoButton != null)
+//            {
+//                startRecoButton.interactable = !waitingForReco && micPermissionGranted;
+//            }
+//            if (outputText != null)
+//            {
+//                outputText.text = message;
+//            }
+//        }
+//    }
+//}
