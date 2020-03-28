fe9614c119dd05ce879ce1f5d6b211d93851282c
GoldenDragonDesigner <SokaGameDesign@gmail.com>
2020-03-28 00:04:03 -0700

    Created a button prefab for changing scenes
    I created a button prefab for changing scenes, Created a credits scene with a button on it to go back to the main menu, created a button in the Lies scene that goes back to the main menu, added the three scenes to the build setting and created a script for changing scenes.

---
+++ SevenVibrations/Assets/Scripts/GUI/SceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [Tooltip("Type the name of the scene you want to change here.  It is case sensitive")]
+    public string sceneName;
+
+    public void ChangeScenes()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
