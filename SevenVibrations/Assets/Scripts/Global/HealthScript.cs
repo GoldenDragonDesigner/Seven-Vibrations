95a2c773e403f2ce4556e3639c74a9cc39eabd97
Noah <noah2wilson@gmail.com>
2020-03-30 22:11:46 -0400

    Added a health script to the player character, as well as a cube that will damage the player.
    Added a health script and a cube that will damage the player every 10 seconds. These showcase in the HealthScript and Exampledamagescript how to utilize the script for basic damage. There are no death functions or ui elements yet, this is simply barebones to allow for the creation and testing of the barrier powerup.

---
+++ SevenVibrations/Assets/Scripts/Global/HealthScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public int health;
+    public bool canhurt = true;
+
+    void Start()
+    {
+        health = 10;
+    }
+
+    public void Damage(int damage)
+    {
+        if (canhurt)
+        {
+            health -= damage;
+            Debug.Log("took " + damage + " damage.");
+            Debug.Log("Unit has " + health + " health remaining.");
+        }
+        else return;
+    }
+
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, 10);
+    }
+}
---
+++ SevenVibrations/Assets/Scripts/Global/Sampledamagescript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sampledamagescript : MonoBehaviour
+{
+    GameObject player;
+    public float timer;
+    bool timeup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timer = Mathf.Clamp(timer, 0, 10);
+        if (timer == 0)
+            timeup = true;
+        else timeup = false;
+        DealDamage(1);
+    }
+
+    void DealDamage(int damage)
+    {
+            if(timeup == true)
+        {
+            player.GetComponent<HealthScript>().Damage(1);
+            timer = 10;
+        }
+    }
+
+}
