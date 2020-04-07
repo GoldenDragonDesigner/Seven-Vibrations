9624daf579f9c000f4a7e3d51813e3ac7503d91c
Noah <noah2wilson@gmail.com>
2020-04-06 20:29:44 -0400

    Uploading skeleton of vibration meter script to github

---
+++ SevenVibrations/Assets/Scripts/Player/VibrationMeterScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrationMeterScript : MonoBehaviour
+{
+    public bool useMicrophone;
+    string microphone;
+    float vibration;
+    AudioSource adsource;
+    float[] samples;
+
+
+    void Start()
+    {
+        adsource = GetComponent<AudioSource>();
+
+        if (Microphone.devices.Length > 0)
+        {
+            microphone = Microphone.devices[0].ToString();
+        }
+        else useMicrophone = false;
+    }
+
+    
+    void Update()
+    {
+        Vibrate();
+    }
+
+    void Vibrate()
+    {
+        if (useMicrophone)
+        {
+            Debug.Log("recording");
+           adsource.clip = Microphone.Start(microphone, true, 5, 44100);
+            adsource.loop = true;
+            while (!(Microphone.GetPosition(null) > 0))
+            {
+                adsource.Play();
+                AudioListener.GetOutputData(samples, 0);
+                Debug.Log(samples.ToString());
+            }
+        }
+        else Microphone.End(microphone);
+
+        foreach(float sample in samples)
+        {
+            if(sample > 0)
+            {
+                vibration += Time.deltaTime * 2;
+                vibration = Mathf.Clamp(vibration, 0, 100);
+                Debug.Log("Vibration is at " + vibration.ToString());
+            }
+        }
+    }
+}
